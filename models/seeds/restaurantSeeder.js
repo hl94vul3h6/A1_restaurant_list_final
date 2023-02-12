48960450ac981607ba78de91fd7da8ba3b364755
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-11 23:09:02 -0800

    feat: modify restaurantSeeder and user.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,55 @@
-const Restaurant = require("../restaurant"); // 載入 restaurant model
-const restaurantList = require("../../restaurant.json").results
-
-
+const bcrypt = require("bcryptjs");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const db = require("../../config/mongoose"); // need to below line 5
+const Restaurant = require("../restaurant");
+const User = require("../user");
+const restaurantData = require("../../restaurant.json").results;
+const db = require("../../config/mongoose");
+const SEED_USERS = [
+  {
+    name: "user1",
+    email: "user1@example.com",
+    password: "12345678",
+    restaurantIndex: [0, 1, 2],
+  },
+  {
+    name: "user2",
+    email: "user2@example.com",
+    password: "12345678",
+    restaurantIndex: [3, 4, 5],
+  },
+];
 
 db.once("open", () => {
-  console.log("running restaurantSeeder script...");
-   Restaurant.create(restaurantList)
-     .then(() => {
-       console.log("restaurantSeeder done!");
-       db.close();
-     })
-     .catch((err) => console.log(err));
+  return Promise.all(
+    SEED_USERS.map((user) => {
+      const { restaurantIndex } = user;
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(user.password, salt))
+        .then((hash) =>
+          User.create({
+            name: user.name,
+            email: user.email,
+            password: hash,
+          })
+        )
+        .then((user) => {
+          const restaurants = restaurantIndex.map((index) => {
+            const restaurant = restaurantData[index];
+            restaurant.userId = user._id;
+            return restaurant;
+          });
+          return Restaurant.create(restaurants);
+        })
+        .catch((error) => console.log(error));
+    })
+  )
+    .then(() => {
+      console.log("done");
+      process.exit();
+    })
+    .catch((error) => console.log(error));
 });
