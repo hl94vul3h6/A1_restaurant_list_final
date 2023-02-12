2430c73b4cff82d11eea6bd919cff9b5b2079011
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-11 20:08:19 -0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,14 @@ const home = require('./modules/home')
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
 
+const users = require("./modules/users"); 
+
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/restaurants', restaurants)
+
+router.use("/users", users);
 
 // 匯出路由器
 module.exports = router;
